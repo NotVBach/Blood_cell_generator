6e894ba62b8970130f0edc3cb41e5e0b6c64fa5d
DefinatelyNotBach <bachnv06@gmail.com>
2025-07-25 12:58:58 +0700

    fix image sizing

--- data_loader.py
+++ data_loader.py
@@ -8,7 +8,7 @@ import torchvision.transforms as transforms
 from config import args
 import collections
 
-class LoadDataset(Dataset):
+class DatasetLoader(Dataset):
     def __init__(self, img_dir, ann_file, transform=None):
         self.img_dir = img_dir
         self.transform = transform
@@ -19,7 +19,6 @@ class LoadDataset(Dataset):
         self.labels = {}
         for ann in self.annotations['annotations']:
             img_id = ann['image_id']
-            # Use the first category_id per image for classification
             if img_id not in self.labels:
                 self.labels[img_id] = ann['category_id']
 
@@ -34,24 +33,52 @@ class LoadDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         
-        label = self.labels.get(img_id, 0)  # Default to 0 if no label
+        label = self.labels.get(img_id, 0)
         return image, label
-    
-# Processing path
-def load_noaug_data(data_dir, split):
+
+def load_data(data_dir: str, split='train'):
+    """
+    Load dataset path from json files.
+
+    Parameters
+    ----------
+    data_dir : str
+        dataset's directory path
+    split : str
+        in case there there are other set in future (test, val, train1, train2, ...)
+
+    Returns
+    -------
+    str
+        Images path and annotation path
+    """
     img_dir = os.path.join(data_dir, split)
     ann_file = os.path.join(data_dir, 'annotations', f'{split}.json')
     return img_dir, ann_file
 
-# Processing image 
-def preprocess_data(img_dir, ann_file):
+def preprocess_data(img_dir: str, ann_file: str):
+    """
+    Resize images then generate image and label numpy array
+
+    Parameters
+    ----------
+    img_dir : str
+        images directory path
+    ann_file : str
+        annotation file (json) path
+
+    Returns
+    -------
+    np.array
+        Images and annotation array
+    """
     transform = transforms.Compose([
         transforms.Resize((args['image_size'], args['image_size'])),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
-    dataset = LoadDataset(img_dir, ann_file, transform=transform)
+    dataset = DatasetLoader(img_dir, ann_file, transform=transform)
     images, labels = [], []
     
     for img, lbl in dataset:
@@ -61,22 +88,43 @@ def preprocess_data(img_dir, ann_file):
     images = np.array(images)
     labels = np.array(labels)
     
-    split = 'Train'
-    print(f'{split} images shape: {images.shape}')
-    print(f'{split} labels shape: {labels.shape}')
+    print(f'images shape: {images.shape}')
+    print(f' labels shape: {labels.shape}')
     print(f'Label distribution: {collections.Counter(labels)}')
     
     return images, labels
 
-# Processing image but ouptput is tensor dataset withc batch
-def create_dataloader(img_dir, ann_file, batch_size, num_workers=0):
+def create_dataloader(img_dir, ann_file, batch_size, num_workers: int = 0):
+    """
+    Resize images then generate dataset using torch DataLoader
+
+    Parameters
+    ----------
+    img_dir : str
+        images directory path
+    ann_file : str
+        annotation file (json) path
+    batch_size: int
+        batch_size (can be alter in config)
+    num_workers: int
+        load data in parallel using multiple subprocesses but not gonna use it anyway
+
+    Returns
+    -------
+    torch.utils.data.dataloader.DataLoader
+        torch like dataset
+    """
     transform = transforms.Compose([
         transforms.Resize((args['image_size'], args['image_size'])),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
-    dataset = LoadDataset(img_dir, ann_file, transform=transform)
+    dataset = DatasetLoader(img_dir, ann_file, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    print("Load data successfully... \n")
+    
+    # Verify batch shapes
+    # for images, labels in dataloader:
+    #     print(f'Batch images shape: {images.shape}, labels shape: {labels.shape}')
+    #     break  
     return dataloader
