fa687435ae57940d61179b79ca1259974247111a
DefinatelyNotBach <bachnv06@gmail.com>
2025-08-03 23:47:00 +0700

    init main

---
+++ main.py
@@ -0,0 +1,34 @@
+import argparse
+from config import args
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description='Run Deep SMOTE or Balance GAN')
+    parser.add_argument('--method', type=str, choices=['deep_smote', 'balance_gan'], default=args['method'],
+                        help='Method to run: deep_smote or balance_gan')
+    parser.add_argument('--mode', type=str, choices=['train', 'generate'], default='train',
+                        help='Mode: train or generate')
+    parser.add_argument('--image_size', type=int, nargs=2, default=args['image_size'],
+                        help='Image size as height width (e.g., 32 32)')
+    args_cmd = parser.parse_args()
+
+    args['method'] = args_cmd.method
+    args['train'] = args_cmd.mode == 'train'
+    args['image_size'] = tuple(args_cmd.image_size)
+    args['output_dir'] = os.path.join(args['base_dir'], 'synthetic', args['method'])
+
+    if args['method'] == 'deep_smote':
+        if args['train']:
+            os.system('python deep_smote/train.py')
+        else:
+            os.system('python deep_smote/generate.py')
+    elif args['method'] == 'balance_gan':
+        if args['train']:
+            os.system('python balance_gan/train.py')
+        else:
+            os.system('python balance_gan/generate.py')
+    else:
+        raise ValueError(f"Unknown method: {args['method']}")
+
+if __name__ == '__main__':
+    main()
