cb839c871959117119ca55996151041e490b684f
NotVBach <bachnv06@gmail.com>
2025-08-13 15:00:27 +0700

    remove bbox

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ args = {
     'dim_h': 64,                # factor controlling size of hidden layers
     'n_channel': 3,             # number of channels
     'n_z': 300,                 # number of dimensions in latent space
-    'bbox_dim': 4,              # bounding box dimension
+    # 'bbox_dim': 4,              # bounding box dimension
     'sigma': 1.0,               # variance in n_z
     'lambda': 0.01,             # hyper param for weight of discriminator loss
-    'bbox_lambda': 0.1,         # bounding box hyper param
+    # 'bbox_lambda': 0.1,         # bounding box hyper param
     'lr': 0.0002,               # learning rate for auto encoder Adam optimizer
     'epochs': 200,              # how many epochs to run for training
     'batch_size': 100,          # batch size for SGD
     'save': True,               # save weights at each epoch if True
     'dataset': 'defect',        # specify the dataset
     'base_dir': 'noaug',        # path to dataset
+    'output_dir': '',           # path to output
     'image_size': (128, 128),   # resize images to this size
     'disc_lr': 0.0002,          # learning rate for GAN discriminator
     'num_classes': 8,           
